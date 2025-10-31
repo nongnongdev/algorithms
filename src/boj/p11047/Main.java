ce0a991994af3340fdd305805fb6195674216f27
nongnongdev <ekgns113@gmail.com>
2025-10-31 14:19:49 +0900

    [백준, 11047번  동전 0(실버4) 그리디 알고리즘 활용

---
+++ src/boj/p11047/Main.java
@@ -0,0 +1,32 @@
+package boj.p11047;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] coins = new int[N];
+        for(int i = 0; i < N; i++)
+        {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+        for(int i = N - 1; i >= 0; i--)
+        {
+            if(coins[i] <= K)
+            {
+                count += K / coins[i];
+                K = K % coins[i];
+            }
+        }
+
+        System.out.println(count);
+    }
+}
