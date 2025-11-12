98db5047d859357fb83e9d47b72ef6bad2f38725
nongnongdev <ekgns113@gmail.com>
2025-11-12 22:58:16 +0900

    [백준] 14501번 퇴사 - JAVA[자바] (실버3) - DP Bottom-up이지만 배열의 마지막에서 시작

---
+++ src/boj/p14501/Main.java
@@ -0,0 +1,37 @@
+package boj.p14501;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] D = new int[N + 2];
+        int[] T = new int[N + 1];
+        int[] P = new int[N + 1];
+        for(int i = 1; i <= N; i++)
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = N; i > 0; i--)
+        {
+            if(i + T[i] > N + 1)
+            {
+                D[i] = D[i + 1];
+            }
+            else
+            {
+                D[i] = Math.max(D[i + 1], P[i] + D[i + T[i]]);
+            }
+        }
+
+
+        System.out.println(D[1]);
+    }
+}
