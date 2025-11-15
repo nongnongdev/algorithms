9b9f94d03d8c31e2edcda65ec59c07fcfda614e3
nongnongdev <ekgns113@gmail.com>
2025-11-15 22:58:31 +0900

    [백준] 9461번 파도반 수열 - JAVA[자바] (골드5) - DP Bottom-up 방식으로 풀이, 파도반 수열 이용 F[N] = F[N - 2] + F[N - 3];

---
+++ src/boj/p9461/Main.java
@@ -0,0 +1,28 @@
+package boj.p9461;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        long[] dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        for(int i = 4; i < 101; i++)
+        {
+            dp[i] = dp[i - 2] + dp[i - 3];
+        }
+        while(T-- > 0)
+        {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(dp[N]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
