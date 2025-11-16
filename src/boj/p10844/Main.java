0e2273bdcc0e21a50a27ba70cc57f5c808fc9625
nongnongdev <ekgns113@gmail.com>
2025-11-16 23:39:46 +0900

    [백준] 10844번 쉬운 계단 수 - JAVA[자바] (실버1) - DP의 Bottom-up으로 풀이(점화식 도출 연습 문제로 활용할 것)

---
+++ src/boj/p10844/Main.java
@@ -0,0 +1,36 @@
+package boj.p10844;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long mod = 1000000000;
+        int N = Integer.parseInt(br.readLine());
+        long[][] D = new long[N + 1][11];
+        for(int i = 1; i <= 9; i++)
+        {
+            D[1][i] = 1;
+        }
+
+        for(int i = 2; i <= N; i++)
+        {
+            D[i][0] = D[i - 1][1];
+            D[i][9] = D[i - 1][8];
+            for(int j = 1; j <= 8; j++)
+            {
+                D[i][j] = (D[i - 1][j - 1] + D[i - 1][j + 1]) % mod;
+            }
+        }
+
+        long sum = 0;
+        for(int i = 0; i < 10; i++)
+        {
+            sum = (sum + D[N][i]) % mod;
+        }
+
+        System.out.println(sum);
+    }
+}
