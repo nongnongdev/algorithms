ca1b523eae1a2e2361c4a774f3bbc33db6e90170
nongnongdev <ekgns113@gmail.com>
2025-11-17 22:34:10 +0900

    [백준] 9251번 LCS - JAVA[자바] (골드4) - DP의 Bottom-up으로 풀이(문자열 처리 문제 LCS 2에서 문자열 역추적만 뺀 문제)

---
+++ src/boj/p9251/Main.java
@@ -0,0 +1,30 @@
+package boj.p9251;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] A = br.readLine().toCharArray();
+        char[] B = br.readLine().toCharArray();
+        int[][] dp = new int[A.length + 1][B.length + 1];
+        for(int i = 1; i <= A.length; i++)
+        {
+            for(int j = 1; j <= B.length; j++)
+            {
+                if(A[i - 1] == B[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else
+                {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[A.length][B.length]);
+    }
+}
